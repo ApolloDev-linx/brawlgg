eb4e14f24c354f9036fb34e2b0036f80c30cde9b
ApolloDev-linx <bossangel1127@gmail.com>
2026-04-20 17:51:24 -0500

    add: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
@@ -21,7 +22,10 @@ export default function RootLayout({
         className={`${GeistSans.variable} ${GeistMono.variable} antialiased min-h-screen`}
       >
         <Navigation />
-        <main className="max-w-5xl mx-auto px-4 pb-16">{children}</main>
+        <main className="max-w-5xl mx-auto px-4 pb-16">{children}
+
+	</main>
+	<Analytics />
       </body>
     </html>
   );
